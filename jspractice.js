0e07412c2ee304117907a8c4460c96e00fa792b1
Luisa Salcedo <profe.salcedo@gmail.com>
2017-03-09 16:19:59 -0600

    added code I practice on repl

---
+++ jspractice.js
@@ -0,0 +1,137 @@
+
+function add (num1, num2){
+  return (num1 + num2)
+  
+}
+
+function subtract (num1, num2){
+  return (num1 - num2)
+}
+
+
+console.log(subtract(100, 50))
+
+
+// Don't touch this code!
+
+function testAdd() {
+   var num1 = 5;
+   var num2 = 10;
+   var expected = 15;
+
+    if (add(num1, num2) == expected) {
+      console.log('SUCCESS: `testAdd` is working');
+      return true;
+    }
+   else {
+      console.log('FAILURE: `testAdd` is not working');
+      return false;
+   }
+}
+
+testAdd()
+function add (num1, num2){
+  return (num1 + num2)
+}
+console.log(add(100,50))
+
+function subtract (num1, num2){
+  return (num1- num2)
+}
+console.log(subtract(1000,500))
+
+function printThreeCities (city1, city2, city3, city4, city5){
+  console.log(city1, city3, city5)
+}
+printThreeCities("bogota", "chicago","paris","NYC","phoenix")
+
+"Chicago".length
+
+"I want some coffee".length;
+15>9;
+ console.log(2*10);
+ 
+ function printTwoCities(city1,city2){
+   return city1 + " "+ city2;
+ }
+ 
+ (15>4);
+ "Xiao hui".length;
+ "goody donalson".length;
+ (8*2);
+ 
+function printTwo (practice1, practice2, practice3){
+  console.log(practice1, practice3) 
+}
+printTwo ("grit", "perseverance", "joy")
+
+if("luisa".length<20){
+  console.log("you have a short name!");
+}
+
+if("chicago".length<20){
+  console.log("let's go down the first road!");
+}
+else
+{
+  console.log("we are lost");
+}
+
+if("luisa".length<2) {
+  console.log("too short!");
+}
+else
+{
+  console.log("that's more like it!");
+}
+
+if("farwell".length>5) {
+  console.log("that condition is true");
+}
+else {
+  console.log("that condition is false");
+}
+
+prompt("Are you ok?");
+confirm("calls made");
+
+(8>2);
+(12+2);
+
+function add( num1, num2){
+    return num1 + num2
+}
+add (20,10);
+
+"luisa".length
+
+10>2
+8+2===5+5
+5*2===5+5
+
+if("luisa".length<4)
+{
+  console.log("too short!");
+}
+else{
+  console.log("too long")
+} 
+
+if("heather".length<5){
+  console.log("this condition is true");
+}
+else{
+  console.log("this condition is false");
+}
+
+if("julie".length<2*10)
+{
+  console.log("this condition is true");
+}
+else{
+  console.log("this condition is false");
+}
+
+
+
+
