e2c786e3ae5fcb23df23bdde7d9e3ee02fbcde08
Luisa Salcedo <profe.salcedo@gmail.com>
2017-03-28 13:19:38 -0500

    defined my variables and set my loop

--- practice_10.js
+++ practice_10.js
@@ -1,20 +1,42 @@
 //wait for the paget to load and then run this code
+
+
 $(function(){
 	//enter a number, click and get result
 	$("form").submit(function(event) {
 		event.preventDefault()
 		var userNumber = $('input').val()
 		userNumber = parseInt(userNumber)
+		console.log(userNumber)
+		var fizz='fizz';
+		var buzz='buzz';
+		var fizzbuzz='fizzbuzz';
+		fizzCheck(userNumber, fizz, buzz, fizzbuzz)
+
+	});//end of submit function
+
+	person("Max", 29, "teacher");
+
+});
+
+function fizzCheck  (input, fizz, buzz, fizzbuzz){
+
+	for(var i = 0; i < input; i++) {
+		if(input % 3 === 0) {
+			alert(fizz);
+		} else if (input % 5 === 0){
+			alert (buzz);
+		} else if (input % 5 === 0 && input %3 ===0) {
+			alert (fizzbuzz);
+		} else {
+			alert ('not working');
+		}
 		
+	}
 
-	});
+} 
 
-
-});
-
-	//Cat Carousell Answers:
-$(".thumbnail").click(function(event){
-  //explain what the click handler does
-  var imgSource = $(event.currentTarget).find('img').attr('src');
-  $(".hero img").attr('src', imgSource);
-});
+function person(name, age, profession) {
+// Max is 29 years old and is a teacher.
+	console.log(name + ' is ' + age + ' years old and is a ' + profession);
+}
