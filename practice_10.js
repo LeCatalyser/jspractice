930f48daa355a547e92cb486324891620ec1998b
Luisa Salcedo <profe.salcedo@gmail.com>
2017-03-27 19:32:03 -0500

    practicing event listening

--- practice_10.js
+++ practice_10.js
@@ -1,4 +1,20 @@
+//wait for the paget to load and then run this code
 $(function(){
+	//enter a number, click and get result
+	$("form").submit(function(event) {
+		event.preventDefault()
+		var userNumber = $('input').val()
+		userNumber = parseInt(userNumber)
+		
+
+	});
 
 
+});
+
+	//Cat Carousell Answers:
+$(".thumbnail").click(function(event){
+  //explain what the click handler does
+  var imgSource = $(event.currentTarget).find('img').attr('src');
+  $(".hero img").attr('src', imgSource);
 });
