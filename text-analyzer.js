6645615295e0e1660a7345d13117c020cd2ad0a1
Luisa Salcedo <profe.salcedo@gmail.com>
2017-03-31 13:22:31 -0500

    corrected code on html and added the loop

--- text-analyzer.js
+++ text-analyzer.js
@@ -1,1 +1,31 @@
-$(function)
+$(function(){
+	$(".form").submit(function(event){
+		event.preventDefault();
+		var input = $("#user-text").val();
+	textAnalyser(input);
+	});
+});
+
+function textAnalyser(text) {
+	text.split(' ').length;
+	var words = text.split(' ');
+	var uniqueWords = {}
+	var averageWord = 
+	for( var i = 0; i < words.length; i++){
+		var words uniqueWords averageWord= $(this).find("#user-text")
+		return(text.length)
+	}
+}
+
+
+var requirements = ['average','count', 'uniqueWords'];
+
+
+for(var i = 0; i < requirements.length; i++) {
+	if(requirements[i].average === 'average') {
+  	//do average js
+  } if else (requirements[i].count === 'count') {
+  // do count
+  } if else (requirements[i].uniqueWords === 'uniqueWords')
+  // do unique words....
+}
